765a0a0be443f81b08d4b159a376b7a044f65981
kalyuzhin <67906018+kalyuzhin@users.noreply.github.com>
2023-12-27 21:41:49 +0300

    Add files

---
+++ 2.1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Марк Калюжин on 27.12.2023.
+//
+
+#include "2.1.h"
+
+
+// Task
+// https://leetcode.com/problems/minimum-time-to-make-rope-colorful/?envType=daily-question&envId=2023-12-27
---
+++ 2.1.h
@@ -0,0 +1,13 @@
+//
+// Created by Марк Калюжин on 27.12.2023.
+//
+
+#ifndef LEETCODE_2_1_H
+#define LEETCODE_2_1_H
+
+// Task
+// https://leetcode.com/problems/minimum-time-to-make-rope-colorful/?envType=daily-question&envId=2023-12-27
+
+
+
+#endif //LEETCODE_2_1_H
